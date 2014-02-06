e536c7c266c122e962c0d9e902770a07d7c98e3f
Vid <vidmantas.zemleris@gmail.com>
2014-02-06 18:21:16 +0100

    new dasmaps

--- js/clean_das.js
+++
@@ -1,9 +0,0 @@
-// clean-up parser DB
-parser = db.getSisterDB("parser");
-parser.db.drop();
-// clean-up DAS DB
-das = db.getSisterDB("das");
-das.dropDatabase();
-// clean-up mapping DB
-mapping = db.getSisterDB("mapping");
-mapping.dropDatabase();
